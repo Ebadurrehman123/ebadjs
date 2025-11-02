7e2b57108a60bd1c990249cc2c3865de65947a23
Ebadurrehman123 <urrehmanebad01@gmail.com>
2025-11-02 22:06:21 +0500

    div container changes

---
+++ function.js
@@ -0,0 +1,30 @@
+function saymyname(){
+    console.log("ebadurrehman")
+
+}
+//saymyname()
+
+function addtwonumber(number1,number2){
+   // console.log(number1 + number2);
+   //let result=number1+number2
+   //return result
+   //console.log("ebad")
+   return number1+number2
+
+}
+addtwonumber(33,5)
+
+//console.log( "result",result);
+
+function loginusermessage(username="sam"){
+    if (username===undefined){
+        console.log("enter username")
+        return
+
+    }
+    return `${username} just login`
+
+}
+//console.log(loginusermessage())
+console.log(loginusermessage("ebad"))
+  
