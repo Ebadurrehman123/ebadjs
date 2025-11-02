ca5361bbe2525a3ef90ff7c4e66b85ff273367c3
Ebadurrehman123 <urrehmanebad01@gmail.com>
2025-11-02 23:22:10 +0500

    div container changes
    Signed-off-by: Ebadurrehman123 <urrehmanebad01@gmail.com>

--- function.js
+++ function.js
@@ -15,5 +15,32 @@ function loginusermessage(username="sam"){
 
 }
 //console.log(loginusermessage())
-console.log(loginusermessage("ebad"))
-  
+//console.log(loginusermessage("ebad"))
+
+
+function calculatecartprice(val1,val2,...num1){
+    return num1
+
+}
+const user={
+    username:"ebadurrehman",
+    price:444
+}
+function handobject(anyobject){
+  console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+
+//console.log(calculatecartprice(200,33,434))
+//handobject(user)
+handobject({
+    username:"ebad",
+    price:333
+})
+const mynewarray=[200,333,44,555]
+function returnsecondvalue(getarray){
+    return getarray[2]
+
+}
+//console.log(returnsecondvalue(mynewarray));]
+//console.log(returnsecondvalue([22,33,44,55,66,77]))
