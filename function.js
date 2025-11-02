906e8ad960a478f6707a9a259f0766fea68ac413
Ebadurrehman123 <urrehmanebad01@gmail.com>
2025-11-02 22:48:31 +0500

    div containner changes
    Signed-off-by: Ebadurrehman123 <urrehmanebad01@gmail.com>

--- function.js
+++ function.js
@@ -4,17 +4,6 @@ function saymyname(){
 }
 //saymyname()
 
-function addtwonumber(number1,number2){
-   // console.log(number1 + number2);
-   //let result=number1+number2
-   //return result
-   //console.log("ebad")
-   return number1+number2
-
-}
-addtwonumber(33,5)
-
-//console.log( "result",result);
 
 function loginusermessage(username="sam"){
     if (username===undefined){
--- practise.js
+++ practise.js
@@ -1,3 +1,23 @@
-name="ebadurrehman"
-age=22
-console.log(`my name is ${name} and my age is ${age}`)
+//name="ebadurrehman"
+//age=22
+//console.log(`my name is ${name} and my age is ${age}`)
+function addtwonumber(number1,number2){
+    let result=number1+number2
+    //return result
+   // return number1+number2
+    console.log(result)
+
+
+}
+addtwonumber(3,43)
+
+function userlogin(user){
+   // console.log(user)
+   if(user=="ebad"){
+    console.log("welcome ebad")
+
+   }
+
+
+}
+userlogin("ebad")
