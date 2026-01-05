a819f7d43afaba572fa75fccb0283c0b7b5448ea
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-01-05 13:02:05 +0500

    Update thisandarrowfunction.js

--- thisandarrowfunction.js
+++ thisandarrowfunction.js
@@ -28,16 +28,16 @@ const user={
     price:444,
     welcomemessage:function(){
         console.log(`${this.username} welcome to website`)
-       // console.log(this );
+        console.log(this );
     }
 }
-//user.welcomemessage()
-//user.username="sam"
-//user.welcomemessage()
+user.welcomemessage()
+user.username="sam"
+user.welcomemessage()
 console.log(this)
 
 // function
-/*function chai(){
+function chai(){
     let username="ebad"
     console.log(this.username);
 }   
