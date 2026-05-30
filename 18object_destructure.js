7dbdfa7b378f59c90016d8be0a2c97cd5f80e621
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-05-30 17:43:01 +0700

    git commit changes

--- 18object_destructure.js
+++ 18object_destructure.js
@@ -1,8 +1,11 @@
+#destructuring
 const course={
     coursename:"js in urdu",
     price:"222",
     courseInstructor:"hitesh",
 }
+//course.courseInstructore
+//const{courseInstructor}=course
 const {courseInstructor:instructor}=course
 console.log(instructor);
 
