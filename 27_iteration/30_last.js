5ddada6cb970b579f82fc4fe3e92261c921aac93
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-06-13 14:56:29 +0500

    git commit changes

--- 27_iteration/30_last.js
+++ 27_iteration/30_last.js
@@ -7,4 +7,4 @@ const newNums=myNumbers
                 .map((num)=>num*10)
                 .map( (num) => num +1)
                 .filter( (num)=>num>=40)
-console.log(newNums)
+console.log(newNums);
