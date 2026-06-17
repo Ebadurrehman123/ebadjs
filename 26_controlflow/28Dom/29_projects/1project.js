1c32e595ee99ce756e2c103bb8ad25a92bd83df0
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-06-17 13:51:02 +0700

    Update 1project.js

--- 26_controlflow/28Dom/29_projects/1project.js
+++ 26_controlflow/28Dom/29_projects/1project.js
@@ -1,2 +1,13 @@
 const button=document.querySelectorAll('.button')
 const body=document.querySelector('body')
+
+button.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e) {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id==='grey'){
+            body.style.backgroundcolor=e.target.id;
+        }
+    })
+});
