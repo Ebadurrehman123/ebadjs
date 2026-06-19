b90a16b1d4f49c7e664d7a7c4356c3327076aeae
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-06-19 12:43:28 +0700

    git commit changes

---
+++ 26_controlflow/28Dom/29_projects/2project.js
@@ -0,0 +1,22 @@
+const form=document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseFloat(document.querySelector('#weight').value);
+    const result=document.querySelector("#result");
+    if(height===''||height<0||isNaN(height)){
+        result.innerHTML=`pls give a valid height ${height}`;
+    }else if(weight===''||height<0||isNaN(weight)){
+        result.innerHTML=`pls give a valid weight ${weight}`;
+    
+    }else {
+      const bmi=  (weight / ((height * height)/10000)).toFixed(2)
+      //shoow the results
+      result.innerHTML=`<span>${bmi}</span>`;
+    }if(bmi>18){
+        result.innerHTML=`you weight is less than 18${bmi}`
+    } else{
+        result.innerHTML=`${bmi}`;
+    }
+    
+});
