bb75b7ddd6fa352defaa60e6c04b1df1a0166943
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-06-30 10:58:40 +0500

    gitncommitnchanges

--- 31Async_promiseChains_callback_hell/app.js
+++ 31Async_promiseChains_callback_hell/app.js
@@ -7,10 +7,26 @@ function hello(){
 }
 setTimeout(hello,2000)//set timeout;
 //callbacks
+//The setTimeout() method calls a function after a number of milliseconds.
 //ik fuction ka andr dura function 
 // pass krta hai us ko callback katey hai
 //asa function jo dusra function ka andr 
 //as an argument pass hota ho
+
+/*
+The setTimeout() is executed only once.
+
+If you need repeated executions, use setInterval() instead.
+
+Use the clearTimeout() method to prevent the function from starting.
+
+To clear a timeout, use the id returned from setTimeout():
+
+myTimeout = setTimeout(function, milliseconds);
+Then you can to stop the execution by calling clearTimeout():
+
+*/
+
 function sum(a,b){
     console.log(a+b);
 }
